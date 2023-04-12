041288c6e78aec8b9983baba9aa6f0e6db507032
muaz-aldairi <59302571+muaz-aldairi@users.noreply.github.com>
2023-04-13 11:57:30 +1200

    Add project files.

---
+++ BirdRecogniser02/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using BirdRecogniser02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BirdRecogniser02.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Mobile()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BirdRecogniser02/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BirdRecogniser02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
