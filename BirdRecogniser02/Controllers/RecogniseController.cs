01651f7913a4a863b9cd2de721b127da870cfdda
muaz-aldairi <59302571+muaz-aldairi@users.noreply.github.com>
2023-04-14 00:54:21 +1200

    Creating Recognize controller and view, connecting Recognise to the home page, uploading photo form with preview and reset functions

---
+++ BirdRecogniser02/Controllers/RecogniseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BirdRecogniser02.Controllers
+{
+    public class RecogniseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
