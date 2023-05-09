412e84f10ff4ad9b8f397db59f454ae404ecb7b1
Hong Wang <Hong.Wang01@student.weltec.ac.nz>
2023-05-09 21:53:52 +1200

    can view 3 predictions and probability

--- BirdRecogniser02/Controllers/ResultController.cs
+++ BirdRecogniser02/Controllers/ResultController.cs
@@ -5,14 +5,20 @@ namespace BirdRecogniser02.Controllers
     {
         public class ResultController : Controller
         {
-        public IActionResult Index(string prediction, string probability, string croppedImageDataUrl)
+        [HttpPost]
+        public IActionResult Index(string prediction0, string probability0, string prediction1, string probability1, string prediction2, string probability2, string croppedImageDataUrl)
         {
-            ViewData["Prediction"] = prediction;
-            ViewData["Probability"] = probability;
+            ViewData["Prediction0"] = prediction0;
+            ViewData["Probability0"] = probability0;
+            ViewData["Prediction1"] = prediction1;
+            ViewData["Probability1"] = probability1;
+            ViewData["Prediction2"] = prediction2;
+            ViewData["Probability2"] = probability2;
             ViewData["CroppedImageDataUrl"] = croppedImageDataUrl;
 
             return View();
         }
+
 
     }
 }
