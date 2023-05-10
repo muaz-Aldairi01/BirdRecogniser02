248de9d63e9371ddbd7f91b12dc6c3e9e582e15e
muaz-aldairi <59302571+muaz-aldairi@users.noreply.github.com>
2023-05-11 07:23:38 +1200

    index method test

--- BirdRecogniser02UnitTest/SubmissionsUnitTest.cs
+++ BirdRecogniser02UnitTest/SubmissionsUnitTest.cs
@@ -1,6 +1,17 @@
-﻿using System;
+﻿using BirdRecogniser02.Authorization;
+using BirdRecogniser02.Controllers;
+using BirdRecogniser02.Data;
+using BirdRecogniser02.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +20,60 @@ namespace BirdRecogniser02UnitTest
     [TestClass]
     public class SubmissionsUnitTest
     {
+        [TestMethod]
+        public async Task Index_ReturnsAViewResult_WithAListOfSubmissions()
+        {
+        //    // Arrange
+        //    var mockContext = new Mock<ApplicationDbContext>();
+        //    mockContext.Setup(sub=> sub.ListAsync())
+        //.ReturnsAsync(GetTestSubmissions());
+        //    var mockEnv = new Mock<IWebHostEnvironment>();
+        //    var mockAuth = new Mock<IAuthorizationService>();
+        //    var mockUserManager = new Mock<UserManager<IdentityUser>>(Mock.Of<IUserStore<IdentityUser>>(), null, null, null, null, null, null, null, null);
+        //    var controller = new SubmissionsController(mockContext.Object,mockEnv.Object,mockAuth.Object, mockUserManager.Object);
+        //    controller.ControllerContext = new ControllerContext
+        //    {
+        //        HttpContext = new DefaultHttpContext
+        //        {
+        //            User = new ClaimsPrincipal(new ClaimsIdentity(new[]
+        //            {
+        //        new Claim(ClaimTypes.Role, Constants.SubmissionManagersRole),
+        //        //new Claim(ClaimTypes.Role, Constants.SubmissionAdminstratorsRole)
+        //    }))
+        //        }
+        //    };
+
+        //    // Act
+        //    var result = await controller.Index();
+
+        //    // Assert
+        //   //Assert.IsTypeOf<ViewResult>(result);
+        //    var viewResult = (ViewResult)result;
+        //    Assert.IsNull(viewResult.ViewName); // Optional: Check the view name if not the default view
+        //    Assert.IsNotNull(viewResult.Model); // Optional: Check if the model is not null
+
+        //}
+
+        //private List<Submission> GetTestSubmissions()
+        //{
+        //    var submissions = new List<Submission>();
+        //    submissions.Add(new Submission()
+        //    {
+        //        BirdName = "King Fisher",
+        //        BirdInformation = " blue and orange bird ",
+        //        FileName = "kingfisher-2046453__340.jpg",
+        //        Status = SubmissionStatus.Approved,
+        //        OwnerID = "manager@birdrecogniser.com"
+        //    });
+        //    submissions.Add(new Submission()
+        //    {
+        //        BirdName = "New bird",
+        //        BirdInformation = " mix colors bird ",
+        //        FileName = "NewBird.jpeg",
+        //        Status = SubmissionStatus.Submitted,
+        //        OwnerID = "manager@birdrecogniser.com"
+        //    });
+        //    return submissions;
+        }
     }
 }
