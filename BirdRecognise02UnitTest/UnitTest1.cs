525ad6d4a4b00540d0d0a5e18b0f1476a072bf9e
muaz-aldairi <59302571+muaz-aldairi@users.noreply.github.com>
2023-05-12 09:30:24 +1200

    trying xUnit testing, but did not work(error in userManager service)

--- BirdRecognise02UnitTest/UnitTest1.cs
+++ BirdRecognise02UnitTest/UnitTest1.cs
@@ -1,4 +1,3 @@
-using BirdRecogniser02.Authorization;
 using BirdRecogniser02.Controllers;
 using BirdRecogniser02.Data;
 using BirdRecogniser02.Models;
@@ -7,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Controller;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using System.Security.Claims;
 
@@ -44,6 +43,98 @@ namespace BirdRecognise02UnitTest
         //    Assert.Equal(submissionList.Count, model.Count());
         //}
 
+
+        //================================================================================
+
+
+        //private readonly ApplicationDbContext _context;
+        //private readonly IWebHostEnvironment _hostEnvironment;
+        //private readonly IAuthorizationService _authorizationService;
+        //private readonly Mock<UserManager<IdentityUser>> _userManager;
+
         
+        //public UnitTest1()
+        //{
+        //    // Initialize any dependencies required for the controller
+        //    _context = new ApplicationDbContext(new DbContextOptionsBuilder<ApplicationDbContext>()
+        //        .UseInMemoryDatabase("TestDatabase")
+        //        .Options);
+        //    _hostEnvironment = Mock.Of<IWebHostEnvironment>();
+        //    _authorizationService = Mock.Of<IAuthorizationService>();
+        //    _userManager = new Mock<UserManager<IdentityUser>>(MockBehavior.Strict);
+        //}
+
+        //[Fact]
+        //public async Task Index_ReturnsViewResultWithSubmissions()
+        //{
+        //    // Arrange
+        //    var controller = new SubmissionsController(_context, _hostEnvironment, _authorizationService, _userManager);
+
+        //    // Act
+        //    var result = await controller.Index();
+
+        //    // Assert
+        //    var viewResult = Assert.IsType<ViewResult>(result);
+        //    var model = Assert.IsAssignableFrom<IEnumerable<Submission>>(viewResult.ViewData.Model);
+        //    Assert.NotNull(model);
+        //}
+
+        //[Fact]
+        //public async Task Details_WithValidId_ReturnsViewResultWithSubmission()
+        //{
+        //    // Arrange
+        //    var submissionId = 1;
+        //    var submission = new Submission { SubmissionId = submissionId };
+        //    _context.Submission.Add(submission);
+        //    _context.SaveChanges();
+        //    var controller = new SubmissionsController(_context, _hostEnvironment, _authorizationService, _userManager);
+
+        //    // Act
+        //    var result = await controller.Details(submissionId);
+
+        //    // Assert
+        //    var viewResult = Assert.IsType<ViewResult>(result);
+        //    var model = Assert.IsType<Submission>(viewResult.ViewData.Model);
+        //    Assert.Equal(submissionId, model.SubmissionId);
+        //}
+
+        //[Fact]
+        //public async Task Details_WithNullId_ReturnsNotFoundResult()
+        //{
+        //    // Arrange
+        //    int? submissionId = null;
+        //    var controller = new SubmissionsController(_context, _hostEnvironment, _authorizationService, _userManager);
+
+        //    // Act
+        //    var result = await controller.Details(submissionId);
+
+        //    // Assert
+        //    Assert.IsType<NotFoundResult>(result);
+        //}
+
+        //[Fact]
+        //public async Task Details_WithUnauthorizedUser_ReturnsForbidResult()
+        //{
+        //    // Arrange
+        //    var submissionId = 1;
+        //    var submission = new Submission { SubmissionId = submissionId };
+        //    _context.Submission.Add(submission);
+        //    _context.SaveChanges();
+        //    var controller = new SubmissionsController(_context, _hostEnvironment, _authorizationService, _userManager);
+        //    controller.ControllerContext = new ControllerContext
+        //    {
+        //        HttpContext = new DefaultHttpContext
+        //        {
+        //            User = new ClaimsPrincipal(new ClaimsIdentity())
+        //        }
+        //    };
+
+        //    // Act
+        //    var result = await controller.Details(submissionId);
+
+        //    // Assert
+        //    Assert.IsType<ForbidResult>(result);
+        //}
+
     }
 }
